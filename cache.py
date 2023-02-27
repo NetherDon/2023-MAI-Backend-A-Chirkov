2c88912bdeff8ad7d86eaa6710d1789b554f05e6
NetherDon <angryzombi2013@gmail.com>
2023-02-27 15:48:03 +0300

    Move LRUCache class to separate file

---
+++ cache.py
@@ -0,0 +1,26 @@
+class LRUCache:
+    def __init__(self, capacity: int=10) -> None:
+        self._cache = {}
+        self.capacity = capacity
+
+    def __len__(self):
+        return len(self._cache)
+
+    def __str__(self):
+        return str(self._cache)
+
+    def get(self, key: str) -> str:
+        value = self._cache.pop(key)
+        self._cache[key] = value
+        return value
+
+    def set(self, key: str, value: str) -> None:
+        self._cache.pop(key, None)
+        if len(self) < self.capacity:
+            self._cache[key] = value
+        else:
+            del self._cache[next(iter(self._cache.keys()))]
+            self._cache[key] = value
+
+    def rem(self, key: str) -> None:
+        del self._cache[key]
